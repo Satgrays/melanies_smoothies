5c751a3e7f839f0cbe75ea759514c0df9e3369e2
Erandi Ramírez <A01656031@itesm.mx>
2025-06-07 05:41:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
+session = get_active_session()
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize your Smoothie :cup_with_straw: ")
 st.write(
